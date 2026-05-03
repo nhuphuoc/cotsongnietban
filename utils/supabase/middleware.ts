dcbfa660cef48032b7e3bc05fa70aaf356de8bb9
Eric Bui <nhuphuoc.bui@gmail.com>
2026-05-03 15:44:55 +0700

    fix bug

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -25,11 +25,15 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => {
+          cookiesToSet.forEach(({ name, value }) => {
             request.cookies.set(name, value);
-            supabaseResponse = NextResponse.next({
-              request,
-            });
+          });
+
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+
+          cookiesToSet.forEach(({ name, value, options }) => {
             supabaseResponse.cookies.set(name, value, options);
           });
         },
